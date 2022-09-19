d6eac6a6088fe8fe5dd5b8adbb4f8823cf00878a
Prathibha <prsthibha@gmail.com>
2022-09-19 14:42:55 +0530

    Add route files with Express Router

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const connectDB = require('./services/mongo')
+const connectDB = require("./services/mongo");
 
 const app = express();
 
@@ -9,9 +9,15 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-app.get('/',(req, res)=>{
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
