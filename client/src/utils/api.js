2d9bc4f8a4c22c2577066d824999808843e4385e
Prathibha <prsthibha@gmail.com>
2022-09-22 09:45:34 +0530

    Create axios instance

---
+++ client/src/utils/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+
+// Create an instance of axios
+const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api;
