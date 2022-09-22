77ea76a9606220bfa595b519ade2c56b022340c4
Prathibha <prsthibha@gmail.com>
2022-09-22 09:14:50 +0530

    Load User & Set Auth Token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,32 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+
+// Load user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const response = await axios.get("/api/auth");
+    dispatch({
+      type: USER_LOADED,
+      payload: response.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // Register user
-
 export const register = (formData) => async (dispatch) => {
   try {
     const response = await axios.post("/api/users", formData);
