88411ab6654fdc18f73a13a906c82df24177f343
Prathibha <prsthibha@gmail.com>
2022-09-25 11:30:05 +0530

    Add Like & Unlike Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import api from "../utils/api";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -9,6 +9,38 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: response.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const response = await api.put(`/posts/like/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: response.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const response = await api.put(`/posts/unlike/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: response.data },
     });
   } catch (err) {
     dispatch({
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,11 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import formatDate from "../../utils/formatDate";
+import { addLike, removeLike } from "../../actions/post";
 
 export const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -20,11 +23,19 @@ export const PostItem = ({
         <div>
           <p className="my-1">{text}</p>
           <p className="post-date">{formatDate(date)}</p>
-          <button type="button" className="btn btn-light">
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={()=>addLike(_id)}
+          >
             <i className="fas fa-thumbs-up"></i>{" "}
             {likes.length > 0 && <span>{likes.length}</span>}
           </button>
-          <button type="button" className="btn btn-light">
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={()=>removeLike(_id)}
+          >
             <i className="fas fa-thumbs-down"></i>
           </button>
           <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -53,4 +64,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
