a45987472a9ed4ffda42485d4cece18d5ff20b76
Prathibha <prsthibha@gmail.com>
2022-09-26 10:44:17 +0530

    Create Docker Image

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'api',
   headers: {
     'Content-Type': 'application/json'
   }
